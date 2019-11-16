f3afdeb2088e6515678ba49234c449985d569013
rokap0127 <r.okap0127@gmail.com>
2019-11-16 11:11:35 +0900

    アルファ値追加

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -163,7 +163,7 @@ void HelloWorld::draw(Renderer *renderer, const Mat4& transform, uint32_t flags)
 	error = glGetError();
 
 	Vec3 pos[4];
-	Vec3 color[4];
+	Vec4 color[4];
 
 	const float x = 0.5f;
 	const float y = 0.5f;
@@ -173,13 +173,13 @@ void HelloWorld::draw(Renderer *renderer, const Mat4& transform, uint32_t flags)
 	pos[2] = Vec3(x, -y, 0);
 	pos[3] = Vec3(x, y, 0);
 
-	color[0] = Vec3(1, 0, 0);
-	color[1] = Vec3(1, 0, 0);
-	color[2] = Vec3(1, 0, 0);
-	color[3] = Vec3(1, 0, 0);
+	color[0] = Vec4(1, 0, 0, 1);
+	color[1] = Vec4(1, 0, 0, 1);
+	color[2] = Vec4(1, 0, 0, 1);
+	color[3] = Vec4(1, 0, 0, 1);
 	glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_POSITION, 3, GL_FLOAT,GL_FALSE, 0, pos);
 
-	glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_COLOR, 3, GL_FLOAT, GL_FALSE, 0, color);
+	glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_COLOR, 4, GL_FLOAT, GL_FALSE, 0, color);
 
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 	error = glGetError();
@@ -190,12 +190,12 @@ void HelloWorld::draw(Renderer *renderer, const Mat4& transform, uint32_t flags)
 	pos[2].x += 0.1f; pos[2].y += 0.1f; 
 	pos[3].x += 0.1f; pos[3].y += 0.1f;
 
-	color[0] = Vec3(0, 0, 1);
-	color[1] = Vec3(0, 0, 1);
-	color[2] = Vec3(0, 0, 1);
-	color[3] = Vec3(0, 0, 1);
+	color[0] = Vec4(0, 0, 1, 1);
+	color[1] = Vec4(0, 0, 1, 1);
+	color[2] = Vec4(0, 0, 1, 1);
+	color[3] = Vec4(0, 0, 1, 1);
 
 	glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_POSITION, 3, GL_FLOAT, GL_FALSE, 0, pos);
-	glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_COLOR, 3, GL_FLOAT, GL_FALSE, 0, color);
+	glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_COLOR, 4, GL_FLOAT, GL_FALSE, 0, color);
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
